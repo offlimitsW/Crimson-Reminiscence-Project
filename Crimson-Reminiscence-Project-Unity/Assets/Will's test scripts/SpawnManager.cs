52ba4de6abc1c750fd5f58874f64cf3afb66ec23
William Nguyen <nnguyen80@student.rccd.edu>
2022-06-05 11:48:32 -0700

    test

---
+++ Crimson-Reminiscence-Project-Unity/Assets/Will's test scripts/SpawnManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public List<GameObject> spawnPoints;
+    public int currentIndex;
+
+
+}
